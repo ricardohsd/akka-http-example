adf861211eb574735a19732310b73d345d166ef2
Ricardo Domingos <ricardo.domingos@fyber.com>
2017-03-21 15:26:57 -0300

    Add tests for Master actor
    Master actor monitors UserRepository and HttpService and it must
    terminates when either one terminates.

---
+++ src/test/scala/com/ricardohsd/webapi/MasterTest.scala
@@ -0,0 +1,42 @@
+package com.ricardohsd.webapi
+
+import akka.actor.{ActorSystem, PoisonPill, Props}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import org.scalatest.concurrent.Eventually
+import org.scalatest.FunSuiteLike
+
+class HttpServiceCrash extends TestKit(ActorSystem())
+  with FunSuiteLike with ImplicitSender with Eventually {
+
+  test("terminates Master actor when HttpService crashes") {
+    val testProbe = TestProbe()
+    val masterRef = system.actorOf(Props(new Master), "user-app-master")
+    val httpService = system.actorSelection("/user/user-app-master/http-service")
+
+    testProbe.watch(masterRef)
+
+    httpService ! PoisonPill
+
+    eventually {
+      testProbe.expectTerminated(masterRef)
+    }
+  }
+}
+
+class UserRepositoryCrash extends TestKit(ActorSystem())
+  with FunSuiteLike with ImplicitSender with Eventually {
+
+  test("terminates Master actor when UserRepository crashes") {
+    val testProbe = TestProbe()
+    val masterRef = system.actorOf(Props(new Master), "user-app-master")
+    val userRepository = system.actorSelection("/user/user-app-master/user-repository")
+
+    testProbe.watch(masterRef)
+
+    userRepository ! PoisonPill
+
+    eventually {
+      testProbe.expectTerminated(masterRef)
+    }
+  }
+}
